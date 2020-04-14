7b1dc3e4ee6a7b84482a94ba10410a400cbe18e0
mrezabarus <mrezabarus87@gmail.com>
2020-04-14 17:33:45 +0700

    Upload K3 Modul 2 versi terbaru sudah di history perslide
    Upload K3 Modul 2 versi terbaru sudah di history perslide

---
+++ K3M2(3)/story_content/user.js
@@ -0,0 +1,633 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6BiThGQoifO":
+        Script1();
+        break;
+      case "6KR584EpH0t":
+        Script2();
+        break;
+      case "6PxmGLoIVgq":
+        Script3();
+        break;
+      case "5WXcuE6forZ":
+        Script4();
+        break;
+      case "6Wkf6Ip7V9Z":
+        Script5();
+        break;
+      case "6mXVo3RaoOh":
+        Script6();
+        break;
+      case "6oOMEHHCKLS":
+        Script7();
+        break;
+      case "6AHpaQ3DHah":
+        Script8();
+        break;
+      case "5XIjp6HLFIm":
+        Script9();
+        break;
+      case "5nG8hupRC7z":
+        Script10();
+        break;
+      case "650w3jTBUZK":
+        Script11();
+        break;
+      case "6gJSKPoP85p":
+        Script12();
+        break;
+      case "6gGohxksYqO":
+        Script13();
+        break;
+      case "6btPBQg2EPV":
+        Script14();
+        break;
+      case "6B8RJ0hnJNn":
+        Script15();
+        break;
+      case "5nxSr59drkL":
+        Script16();
+        break;
+      case "6eX3wWPecLs":
+        Script17();
+        break;
+      case "6iXAh0IarTe":
+        Script18();
+        break;
+      case "5zQHww0ZkLF":
+        Script19();
+        break;
+      case "5zeGXiwwpGZ":
+        Script20();
+        break;
+      case "6UbprZWOS6H":
+        Script21();
+        break;
+      case "5e8pXmiJ2yi":
+        Script22();
+        break;
+      case "6qGzy1rKbeU":
+        Script23();
+        break;
+      case "6ciZkF3luP2":
+        Script24();
+        break;
+      case "6AGmUydczf5":
+        Script25();
+        break;
+      case "5lTAttSjzpb":
+        Script26();
+        break;
+      case "6Gssy9VGeYp":
+        Script27();
+        break;
+      case "6o2ujnuE9HI":
+        Script28();
+        break;
+      case "5pq9NBNfKEN":
+        Script29();
+        break;
+      case "6qGTcI24E83":
+        Script30();
+        break;
+      case "6DfFSlRxxzm":
+        Script31();
+        break;
+      case "5W74bXlAeY8":
+        Script32();
+        break;
+      case "5nhMOnXbxos":
+        Script33();
+        break;
+      case "5Ut51UpiGfy":
+        Script34();
+        break;
+      case "5pZUBY05XXe":
+        Script35();
+        break;
+      case "5XJNmFN7L7u":
+        Script36();
+        break;
+      case "5aqgM7PWSDx":
+        Script37();
+        break;
+      case "6rGeEAkBwGe":
+        Script38();
+        break;
+      case "5aKuIl3ktzn":
+        Script39();
+        break;
+      case "6RcqgCzSAHc":
+        Script40();
+        break;
+      case "5kZftYzZtpK":
+        Script41();
+        break;
+      case "61WiVHOJHDe":
+        Script42();
+        break;
+      case "6NpN26GVqYV":
+        Script43();
+        break;
+      case "5zpRDe0Yynp":
+        Script44();
+        break;
+      case "6CswtkmjU7T":
+        Script45();
+        break;
+      case "6BtCgXBYceJ":
+        Script46();
+        break;
+      case "5a2r8fxslBq":
+        Script47();
+        break;
+      case "5hBGpL3NMNf":
+        Script48();
+        break;
+      case "6TEw60mbIWh":
+        Script49();
+        break;
+      case "6GxJDNjzKxL":
+        Script50();
+        break;
+      case "6lbsdHyh7YE":
+        Script51();
+        break;
+      case "5fC22JA9Uzi":
+        Script52();
+        break;
+      case "6efrCmJqCac":
+        Script53();
+        break;
+      case "6q8wemPetNp":
+        Script54();
+        break;
+      case "6qWzHohDtPg":
+        Script55();
+        break;
+      case "6LZzcWLMQ9r":
+        Script56();
+        break;
+      case "66sBKracIID":
+        Script57();
+        break;
+      case "5l54Mpo955D":
+        Script58();
+        break;
+      case "62TezXnFXwG":
+        Script59();
+        break;
+  }
+}
+
+function Script1()
+{
+  console.log("page 1");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script2()
+{
+  console.log("page 2");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script3()
+{
+  console.log("page 3");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script4()
+{
+  console.log("page 4");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script5()
+{
+  console.log("page 5");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+/*
+function Script6()
+{
+  
+  console.log("h1.1");
+}
+*/
+function Script7()
+{
+  console.log("page 6");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+/*
+function Script8()
+{
+  console.log("h1.2");
+}
+*/
+function Script9()
+{
+  console.log("page 7");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script10()
+{
+  console.log("h1.3");
+}
+
+function Script11()
+{
+  console.log("page 8");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script12()
+{
+  console.log("h1.4");
+}
+
+function Script13()
+{
+  console.log("page 9");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script14()
+{
+  console.log("h1.5");
+}
+*/
+
+function Script15()
+{
+  console.log("page 10");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script16()
+{
+  console.log("h1.6");
+}
+*/
+
+function Script17()
+{
+  console.log("page 11");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script18()
+{
+  console.log("page 12");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+
+function Script19()
+{
+  console.log("q1.1");
+}
+
+
+function Script20()
+{
+  console.log("page 13");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script21()
+{
+  console.log("q1.2");
+}
+*/
+
+function Script22()
+{
+  console.log("page 14");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script23()
+{
+  console.log("q1.3");
+}
+*/
+
+function Script24()
+{
+  console.log("page 15");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script25()
+{
+  console.log("q1.4");
+}
+*/
+
+function Script26()
+{
+  console.log("page 16");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script27()
+{
+  console.log("q1.5");
+}
+*/
+
+function Script28()
+{
+  console.log("page 17");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script29()
+{
+  console.log("q1");
+console.log("c1");
+}
+*/
+
+function Script30()
+{
+  console.log("page 18");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script31()
+{
+  console.log("page 19");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script32()
+{
+  console.log("page 20");
+
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script33()
+{
+  console.log("h2.1");
+}
+*/
+
+function Script34()
+{
+  console.log("page 21");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script35()
+{
+  console.log("h2.2");
+}
+*/
+
+function Script36()
+{
+  console.log("page 22");
+
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script37()
+{
+  console.log("page 23");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script38()
+{
+  console.log("h2.3");
+}
+*/
+
+function Script39()
+{
+  console.log("page 24");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script40()
+{
+  console.log("h2.4");
+}
+*/
+
+function Script41()
+{
+  console.log("page 25");
+
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script42()
+{
+  console.log("page 26");
+
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script43()
+{
+  console.log("h2.5");
+}
+*/
+
+function Script44()
+{
+  console.log("page 27");
+
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+
+function Script45()
+{
+  console.log("h2.6");
+}
+
+
+function Script46()
+{
+  console.log("page 28");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script47()
+{
+  console.log("page 29");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script48()
+{
+  console.log("q2.1");
+}
+*/
+
+function Script49()
+{
+  console.log("page 30");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script50()
+{
+  console.log("q2.2");
+}
+*/
+
+function Script51()
+{
+  console.log("page 31");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script52()
+{
+  console.log("page 32");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script53()
+{
+  console.log("q2.4");
+}
+*/
+
+function Script54()
+{
+  console.log("page 33");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script55()
+{
+  console.log("q2.5");
+}
+*/
+
+function Script56()
+{
+  console.log("page 34");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+/*
+function Script57()
+{
+  console.log("q2");
+console.log("c2");
+}
+*/
+
+function Script58()
+{
+  console.log("page 35");
+  var player = GetPlayer();
+var query = window.location.search.substring(7);
+player.SetVar("JumpToSlide",query);
+}
+
+function Script59()
+{
+  console.log("page 36");
+  console.log("c2");
+var status = "posttest";
+return status;
+}
+
